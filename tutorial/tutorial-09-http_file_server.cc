91e040abac660fb7c4ef5c75e13bdeedf4358192
401-ubuntu <240450828@qq.com>
2024-08-28 10:47:48 +0800

    update http file server

--- tutorial/tutorial-09-http_file_server.cc
+++ tutorial/tutorial-09-http_file_server.cc
@@ -23,16 +23,36 @@
 #include <stdio.h>
 #include <utility>
 #include <string>
+#include <sstream>
 #include "workflow/HttpMessage.h"
 #include "workflow/HttpUtil.h"
 #include "workflow/WFHttpServer.h"
 #include "workflow/WFTaskFactory.h"
 #include "workflow/Workflow.h"
 #include "workflow/WFFacilities.h"
+#include "StringUtil.h"
 
 using namespace protocol;
 
-void pread_callback(WFFileIOTask *task)
+static const size_t FILE_SIZE_BOOTOM = 10*1024*1024;
+
+void pwitevio_callback(WFFileVIOTask *task)
+{
+	long ret = task->get_retval();
+	HttpResponse *resp = (HttpResponse *)task->user_data;
+
+	if (task->get_state() != WFT_STATE_SUCCESS || ret < 0)
+	{
+		resp->set_status_code("503");
+		resp->append_output_body("503 Internal Server Error.");
+	}
+	else 
+	{
+		resp->set_status_code("200");
+	}
+}
+
+void pwrite_callback(WFFileIOTask *task)
 {
 	FileIOArgs *args = task->get_args();
 	long ret = task->get_retval();
@@ -42,50 +62,236 @@ void pread_callback(WFFileIOTask *task)
 	if (task->get_state() != WFT_STATE_SUCCESS || ret < 0)
 	{
 		resp->set_status_code("503");
-		resp->append_output_body("<html>503 Internal Server Error.</html>");
+		resp->append_output_body("503 Internal Server Error.");
 	}
-	else /* Use '_nocopy' carefully. */
-		resp->append_output_body_nocopy(args->buf, ret);
+	else 
+	{
+		resp->set_status_code("200");
+	}
 }
 
-void process(WFHttpTask *server_task, const char *root)
+void pread_callback(WFFileIOTask *task)
 {
-	HttpRequest *req = server_task->get_req();
-	HttpResponse *resp = server_task->get_resp();
-	const char *uri = req->get_request_uri();
-	const char *p = uri;
+	FileIOArgs *args = task->get_args();
+	long ret = task->get_retval();
+	HttpResponse *resp = (HttpResponse *)task->user_data;
 
-	printf("Request-URI: %s\n", uri);
-	while (*p && *p != '?')
-		p++;
-
-	std::string abs_path(uri, p - uri);
-	abs_path = root + abs_path;
-	if (abs_path.back() == '/')
-		abs_path += "index.html";
-
-	resp->add_header_pair("Server", "Sogou C++ Workflow Server");
-
-	int fd = open(abs_path.c_str(), O_RDONLY);
-	if (fd >= 0)
+	if (task->get_state() != WFT_STATE_SUCCESS || ret < 0)
 	{
-		size_t size = lseek(fd, 0, SEEK_END);
+		resp->set_status_code("503");
+		resp->append_output_body("503 Internal Server Error.");
+	}
+	else /* Use '_nocopy' carefully. */
+	{
+		resp->append_output_body_nocopy(args->buf, ret);
+	}
+}
+
+std::string get_etag(const std::string& path)
+{
+	return path;
+}
+
+inline bool is_digit(char c) 
+{
+	return '0' <= c && c <= '9';
+}
+
+void download_range(WFHttpTask *server_task,  int fd, const std::string& range)
+{
+	HttpResponse *resp = server_task->get_resp();
+	//parser & check range
+	// Range: bytes=89-999
+	size_t start = 0, end = 0;
+	do
+	{
+		if(!range.empty())
+		{
+			const char* c = range.c_str();
+			if(strncmp(range.c_str(),"bytes=",6) != 0)
+				break;
+			c+=6;
+
+			if(!is_digit(*c)) break;
+
+			while(is_digit(*c))
+			{
+				start = start*10+(*c-'0');
+				++c;
+			}
+			if(*c != '-' || !is_digit(*(++c))) break;
+
+			while(is_digit(*c))
+			{
+				end = end*10+(*c-'0');
+				++c;
+			}
+		}
+	} while (0);
+	size_t file_size = lseek(fd, 0, SEEK_END);
+	if(end >= start && end <= file_size && end > 0)
+	{
+		if((end - start) > FILE_SIZE_BOOTOM)
+		{
+			end = start+FILE_SIZE_BOOTOM;
+		}
+		end = std::min(end, file_size);
+		// resp : Content-Range: bytes 89-999/100000
+		std::stringstream ss;
+		ss << "bytes " << start << '-' << end << '/' << file_size;
+		resp->set_header_pair("Content-Range", ss.str());
+		size_t size = end - start;
 		void *buf = malloc(size); /* As an example, assert(buf != NULL); */
 		WFFileIOTask *pread_task;
 
-		pread_task = WFTaskFactory::create_pread_task(fd, buf, size, 0,
-													  pread_callback);
+		pread_task = WFTaskFactory::create_pread_task(fd, buf, size, start,
+														pread_callback);
 		/* To implement a more complicated server, please use series' context
-		 * instead of tasks' user_data to pass/store internal data. */
-		pread_task->user_data = resp;	/* pass resp pointer to pread task. */
+			* instead of tasks' user_data to pass/store internal data. */
+		pread_task->user_data = server_task->get_resp();	/* pass resp pointer to pread task. */
 		server_task->user_data = buf;	/* to free() in callback() */
 		server_task->set_callback([](WFHttpTask *t){ free(t->user_data); });
 		series_of(server_task)->push_back(pread_task);
 	}
 	else
 	{
+		resp->set_status_code("400");
+		resp->append_output_body("Range Error.");
+	}
+}
+
+void download_large_file(WFHttpTask *server_task,int fd, const std::string& abs_path) 
+{
+	HttpRequest *req = server_task->get_req();
+	HttpResponse *resp = server_task->get_resp();
+
+	std::string name;
+	std::string value;
+	std::string range, old_etag;
+	HttpHeaderCursor req_cursor(req);
+	while (req_cursor.next(name, value))
+	{
+		if(name == "If-Range") 
+		{
+			old_etag = value;
+		}
+		if(name == "Range") 
+		{
+			range = value;
+		}
+		if(!old_etag.empty() && !range.empty())
+			break;
+	}
+	std::string now_etag = get_etag(abs_path);
+	resp->set_header_pair("ETag", now_etag);
+	resp->set_header_pair("Accept-Ranges", "bytes");
+	resp->set_header_pair("Content-Type", "application/octet-stream");
+
+	if(!old_etag.empty() && old_etag == now_etag) {
+		//按客户端的range续上
+		resp->set_status_code("206");
+		return download_range(server_task, fd, range);
+	} else {
+		//文件已经改动了,告诉客户端需要从头开始下传
+		resp->set_status_code("200");
+		resp->append_output_body("File is large, use ETag/Range.");
+		return;
+	}
+}
+
+
+void download_hole_file(WFHttpTask *server_task,int fd) 
+{
+	size_t size = lseek(fd, 0, SEEK_END);
+	void *buf = malloc(size); /* As an example, assert(buf != NULL); */
+	WFFileIOTask *pread_task;
+
+	pread_task = WFTaskFactory::create_pread_task(fd, buf, size, 0,
+													pread_callback);
+	/* To implement a more complicated server, please use series' context
+		* instead of tasks' user_data to pass/store internal data. */
+	pread_task->user_data = server_task->get_resp();	/* pass resp pointer to pread task. */
+	server_task->user_data = buf;	/* to free() in callback() */
+	server_task->set_callback([](WFHttpTask *t){ free(t->user_data); });
+	series_of(server_task)->push_back(pread_task);
+}
+
+void process_download(WFHttpTask *server_task,  const std::string& abs_path)
+{
+	HttpResponse *resp = server_task->get_resp();
+
+	int fd = open(abs_path.c_str(), O_RDONLY);
+	if (fd >= 0)
+	{
+		series_of(server_task)->set_callback([fd](const SeriesWork *){
+			close(fd);
+		});
+		//如果是小文件,直接下传
+		size_t size = lseek(fd, 0, SEEK_END);
+		if(size <= FILE_SIZE_BOOTOM)
+		{
+			return download_hole_file(server_task, fd);
+		}
+		else 
+		{
+			return download_large_file(server_task, fd, abs_path);
+		}
+	}
+	else
+	{
 		resp->set_status_code("404");
-		resp->append_output_body("<html>404 Not Found.</html>");
+		resp->append_output_body("404 Not Found.");
+	}
+}
+
+void process_upload(WFHttpTask *server_task,  const std::string& abs_path)
+{
+	HttpRequest *req = server_task->get_req();
+	HttpResponse *resp = server_task->get_resp();
+
+	int cnt = 1024;
+	struct iovec *iovec = (struct iovec *)malloc(sizeof(struct iovec)*cnt);
+	memset(iovec, 0, sizeof(struct iovec)*cnt);
+	HttpUtil::decode_chunked_body_to_iovec(req,iovec,cnt);
+	printf("process_upload iovec: %p cnt:%d\n", iovec, cnt);
+	auto pwrite_task = WFTaskFactory::create_pwritev_task(abs_path, iovec,cnt,0,pwitevio_callback);
+
+	pwrite_task->user_data = resp;
+	server_task->user_data = iovec;
+	server_task->set_callback([](WFHttpTask * t){free(t->user_data);});
+	series_of(server_task)->push_back(pwrite_task);
+}
+
+void process(WFHttpTask *server_task, const char *root)
+{
+	HttpRequest *req = server_task->get_req();
+	HttpResponse *resp = server_task->get_resp();
+
+	resp->add_header_pair("Server", "Sogou C++ Workflow Server");
+	const char *uri = req->get_request_uri();
+	const char *p = uri;
+
+	printf("Request %s  URI: %s\n",req->get_method(), uri);
+	while (*p && *p != '?')
+		p++;
+	std::string abs_path(uri, p - uri);
+	resp->add_header_pair("File", abs_path);
+	StringUtil::url_decode(abs_path);
+	abs_path = root + abs_path;
+	
+	if(strcmp(req->get_method(), HttpMethodGet) == 0) //download
+	{ 
+		return process_download(server_task, abs_path);
+	} 
+	else if(strcmp(req->get_method(), HttpMethodPost) == 0) //upload
+	{ 
+		return process_upload(server_task, abs_path);
+	} 
+	else 
+	{
+		HttpResponse *resp = server_task->get_resp();
+		resp->set_status_code("405");
+		resp->append_output_body("405 Method Not Allowed.");
 	}
 }
 
@@ -93,6 +299,7 @@ static WFFacilities::WaitGroup wait_group(1);
 
 void sig_handler(int signo)
 {
+	printf("recv signo:%d\n", signo);
 	wait_group.done();
 }
 
@@ -105,7 +312,6 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	signal(SIGINT, sig_handler);
 
 	unsigned short port = atoi(argv[1]);
 	const char *root = (argc >= 3 ? argv[2] : ".");
@@ -131,45 +337,9 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	/* Test the server. */
-	auto&& create = [&scheme, port](WFRepeaterTask *)->SubTask *{
-		char buf[1024];
-		*buf = '\0';
-		printf("Input file name: (Ctrl-D to exit): ");
-		scanf("%1023s", buf);
-		if (*buf == '\0')
-		{
-			printf("\n");
-			return NULL;
-		}
+	printf("file server start.[%d] root:%s\n", port, root);
 
-		std::string url = scheme + "127.0.0.1:" + std::to_string(port) + "/" + buf;
-		WFHttpTask *task = WFTaskFactory::create_http_task(url, 0, 0,
-									[](WFHttpTask *task) {
-			auto *resp = task->get_resp();
-			if (strcmp(resp->get_status_code(), "200") == 0)
-			{
-				std::string body = protocol::HttpUtil::decode_chunked_body(resp);
-				fwrite(body.c_str(), body.size(), 1, stdout);
-				printf("\n");
-			}
-			else
-			{
-				printf("%s %s\n", resp->get_status_code(), resp->get_reason_phrase());
-			}
-		});
-
-		return task;
-	};
-
-	WFFacilities::WaitGroup wg(1);
-	WFRepeaterTask *repeater;
-	repeater = WFTaskFactory::create_repeater_task(create, [&wg](WFRepeaterTask *) {
-		wg.done();
-	});
-
-	repeater->start();
-	wg.wait();
+	wait_group.wait();
 
 	server.stop();
 	return 0;
